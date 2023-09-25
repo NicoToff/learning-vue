d00d95b9845a56ec56a447b7a0ab3c5fc4aa9694
NicoToff <toffolo01@hotmail.com>
2023-09-25 17:49:00 +0200

    feat: count button

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ref } from "vue";
+export type CounterComponentProps = {
+  center?: boolean;
+};
+const count = ref(0);
+const { center } = defineProps<CounterComponentProps>();
+</script>
+
+<template>
+  <button :class="center ? 'btn-center' : ''" @click="count++">
+    You clicked: {{ count }} time{{ count === 1 ? "" : "s" }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  border: 2px solid var(--vue-green);
+  margin: 0;
+  padding: 5px 10px 5px 10px;
+  border-radius: 5px;
+  width: fit-content;
+  overflow: visible;
+  text-align: inherit;
+  background: var(--vue-gray);
+  cursor: pointer;
+
+  /* inherit font & color from ancestor */
+  color: inherit;
+  font: inherit;
+  font-weight: 600;
+
+  /* Normalize `line-height`. Cannot be changed from `normal` in Firefox 4+. */
+  line-height: normal;
+
+  /* Corrects font smoothing for webkit */
+  -webkit-font-smoothing: inherit;
+  -moz-osx-font-smoothing: inherit;
+
+  /* Corrects inability to style clickable `input` types in iOS */
+  -webkit-appearance: none;
+}
+
+button:focus {
+  outline-offset: 3px;
+}
+
+button.btn-center {
+  margin: 0 auto;
+  display: block;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,60 @@
+<script lang="ts">
+import CounterComponent from "@/components/CounterComponent.vue";
+export default {
+  components: {
+    CounterComponent,
+  },
+};
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>Welcome to your Playground!</h2>
+    <h3>Powerful counter</h3>
+    <p>
+      Thanks to creative design and the power of JavaScript, your browser can now count the number of times
+      you clicked this button. This revolutionary technology has <strong>countless work hours</strong> behind
+      it. Enjoy it to its fullest!
+    </p>
+    <CounterComponent center />
   </div>
 </template>
 
 <style>
+.about {
+  line-height: normal;
+}
+
+.about p {
+  line-height: 1.5rem;
+  padding-bottom: 1rem;
+}
+
+.about > h2 {
+  color: var(--vue-green);
+  font-size: xx-large;
+  font-weight: 600;
+  padding-bottom: 1rem;
+  padding-top: 1rem;
+}
+
+.about > h3 {
+  color: var(--vue-green);
+  font-size: x-large;
+  font-weight: 400;
+  text-decoration-line: underline;
+  text-underline-offset: 6px;
+  padding-bottom: 12px;
+}
+
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .about > h2 {
+    padding-top: 0;
   }
 }
 </style>
